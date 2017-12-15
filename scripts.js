3dfc6f2f5699e343cc2803d1bf7cb92f875c214f
Your Name <you@example.com>
2017-12-15 20:27:11 +0100

    poprawione

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 var guziki = document.getElementsByClassName('button').length;
-console.log(guziki);
-
-for (x = 0; x < guziki; x++) {
-	var tekst = document.getElementsByClassName('button')[x].innerText;
-	console.log(tekst);
+var tekst = document.getElementsByClassName('button');
+for (var x = 0; x < guziki; x++) {
+	console.log(tekst[x].innerText);
 }
